27cc32eabc2f0f01906fca25e0406831092ef8a1
Anderson <anderson.souza@itbam.org.br>
2020-08-05 17:55:16 -0400

    Aula 03 parcial

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,29 +1,82 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
+import api from '../../services/api';
 
 function TeacherList() {
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        console.log(response.data);
+    }
+
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os proffys disponíveis.">
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matérias</label>
-                        <input type="text" id="subject" />
-                    </div>
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select 
+                        name="subject"
+                        label="Matéria"
+                        value={subject}
+                        onChange={e => { setSubject(e.target.value) }}
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Português', label: 'Português'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Inglês', label: 'Inglês'},
+                            {value: 'Ciências', label: 'Ciências'},
+                            {value: 'Geografia', label: 'Geografia'},
+                            {value: 'História', label: 'História'},
+                            {value: 'Ensino religioso', label: 'Ensino religioso'},
+                            {value: 'Física', label: 'Física'},
+                            {value: 'Química', label: 'Química'},
+                            {value: 'Biologia', label: 'Biologia'}
+                        ]}
+                    />
+                    <Select 
+                        name="week-day"
+                        label="Dia da Semana"
+                        value={week_day}
+                        onChange={e => { setWeekDay(e.target.value) }}
+                        options={[
+                            {value: '0', label: 'Domingo'},
+                            {value: '1', label: 'Segunda-feira'},
+                            {value: '2', label: 'Terça-feira'},
+                            {value: '3', label: 'Quarta-feira'},
+                            {value: '4', label: 'Quinta-feira'},
+                            {value: '5', label: 'Sexta-feira'},
+                            {value: '6', label: 'Sábado'},                            
+                        ]}
+                    />
+                    <Input 
+                        type="time"
+                        name="time" 
+                        label="Hora"
+                        value={time}
+                        onChange={e => { setTime(e.target.value) }}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <button type="submit">
+                        Buscar
+                    </button>
                 </form>
             </PageHeader>
 
